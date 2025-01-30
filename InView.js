e5dbb4a37e7915c8104fed2caa30b63d7eca1436
Andy Gee <rehit.it@gmail.com>
2025-01-30 12:34:04 +0800

    Convert InView.js to Vanilla JavaScript

--- InView.js
+++ InView.js
@@ -1,59 +1,63 @@
 /**
+ * InView Vanilla JS
  *
- * InView jQuery Plugin
- *
- * Watch elements on the page (#id elements) and fire a callback function when it is viewed.
- * @author: Vasanth Krishnamoorthy
- *
- * Do What The F*ck You Want To Public License (WTFPL)
- *
+ * Watches elements on the page and fires a callback function when they are viewed.
+ * Author: Adapted from Vasanth Krishnamoorthy's jQuery plugin
+ * License: Do What The F*ck You Want To Public License (WTFPL)
  */
 
-(function ($, window) {
-    var cache = [],
-        inViewItems = 0,
-        $w = $(window);
+(function () {
+    let cache = new Set();
+    let inViewItems = 0;
+    let ticking = false;
 
-    $.fn.inView = function (callback) {
-        if (typeof callback !== 'function') {
-            return;
-        }
-        var $element = $(this),
-        // Used to refer elements being watched on the page.
-            selectorName = $element.selector;
-
-        inViewItems++;
-
-        // Custom event handler
-        $w.on('scroll.inView', throttle(function () {
-            var winHeight = window.innerHeight || $w.height(),	// < IE9 doesn't use window property
-                scrollDepth = $w.scrollTop() + winHeight;
-
-            // Unbind scroll after all elements to be watched have been viewed
-            if (cache.length >= inViewItems) {
-                $w.off('scroll.inView');
-                return;
+    function throttle(callback, delay = 500) {
+        let wait = false;
+        return function () {
+            if (!wait) {
+                callback();
+                wait = true;
+                setTimeout(() => (wait = false), delay);
             }
+        };
+    }
 
-            // Fire callback function
-            if ($element.length && $.inArray(selectorName, cache) === -1 && scrollDepth >= $element.offset().top) {
-                callback.call(this);
-                cache.push(selectorName);
-            }
-        }));
+    function checkInView() {
+        const winHeight = window.innerHeight || document.documentElement.clientHeight;
+        const scrollDepth = window.scrollY + winHeight;
 
-        //  Rate limit scroll event to fire only once in 500ms
-        function throttle(fn) {
-            var wait = false;
-            return function () {
-                if (!wait) {
-                    fn.call();
-                    wait = true;
-                    setTimeout(function () {
-                        wait = false;
-                    }, 500);
+        document.querySelectorAll("[data-inview]").forEach((element) => {
+            if (!cache.has(element) && scrollDepth >= element.getBoundingClientRect().top + window.scrollY) {
+                const callbackName = element.getAttribute("data-inview-callback");
+                if (callbackName && typeof window[callbackName] === "function") {
+                    window[callbackName].call(element);
                 }
-            };
+                cache.add(element);
+            }
+        });
+
+        if (cache.size >= inViewItems) {
+            window.removeEventListener("scroll", onScroll);
         }
+    }
+
+    function onScroll() {
+        if (!ticking) {
+            requestAnimationFrame(() => {
+                checkInView();
+                ticking = false;
+            });
+            ticking = true;
+        }
+    }
+
+    window.inView = function (selector, callback) {
+        if (typeof callback !== "function") return;
+        document.querySelectorAll(selector).forEach((element) => {
+            element.setAttribute("data-inview", "true");
+            element.setAttribute("data-inview-callback", callback.name);
+        });
+        inViewItems++;
+        window.addEventListener("scroll", throttle(onScroll));
     };
-})(jQuery, window);
+})();
